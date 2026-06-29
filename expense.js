ac97037fe7161b99d5408b5ce0d64ca44e5256bb
Saba_Zakeri_Far <szakerifar@my.bcit.ca>
2026-06-29 13:22:19 -0700

    Add expense modal submit logic and filter/settle UI sections

--- expense.js
+++ expense.js
@@ -94,3 +94,64 @@ allMarkAsSettledBtns.forEach((btn) => {
     });
   });
 });
+
+//================================================================================================
+// add an expense logic in modal
+//================================================================================================
+
+// first update the table with added row
+document.getElementById("exp-submit").addEventListener("click", () => {
+  const titleInput = document.getElementById("exp-title");
+  const costInput = document.getElementById("exp-cost");
+  const expenseTitle = titleInput.value;
+  const costAmount = costInput.value;
+  const Allbadges = document.querySelectorAll("#exp-payer span");
+
+  let selectedBadges = [...Allbadges];
+  //save the result of the filter back to the variable - otherwise it will be thorwn away
+  selectedBadges = selectedBadges.filter((badge) =>
+    badge.classList.contains("border-2"),
+  );
+  console.log(selectedBadges);
+
+  const tableBody = document.querySelector("#my_table tbody");
+  let newTableRow = "";
+  newTableRow = `  <tr>
+    <td class="font-medium " contenteditable="true">${expenseTitle}</td>
+    <td  contenteditable="true"><span>$</span>${costAmount}</td>
+    <td><div class="flex flex-wrap gap-1 badgePlaceHolder"></div></td>
+    <td > <button
+                      class="btn btn-ghost btn-xs text-error"
+                      onclick="this.closest('tr').remove()"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3 6h18M8 6V4h8v2M19 6l-1 14H6L5 6"
+                        />
+                      </svg>
+                    </button></td>
+  </tr>`;
+
+  tableBody.insertAdjacentHTML("beforeend", newTableRow);
+  const badgePlaceholder = tableBody.querySelector(".badgePlaceHolder");
+
+  // cloneNode(true) so original badges stay in the modal for next time
+  selectedBadges.forEach((badge) => {
+    badgePlaceholder.appendChild(badge.cloneNode(true));
+  });
+
+  // clearing the modal for the next use
+
+  titleInput.value = "";
+  costInput.value = "";
+  document.getElementById("my_modal_expense").close();
+});
